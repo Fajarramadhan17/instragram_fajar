e2301e29f02dd6298ef47c7fbc5895940d77b5d8
fajar <Fajar>
2024-12-03 15:07:54 +0700

    penambahan wiget baru

--- lib/main.dart
+++ lib/main.dart
@@ -127,6 +127,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              child: Text(''),
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
